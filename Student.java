5bd8637d52d0efe4672407c6e25788aff7a877e2
Lakshmi Chandana Vupputuri <87367969+ChandanaVupputuri@users.noreply.github.com>
2021-10-06 14:48:57 +0100

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,18 @@
+public class Student
+{
+    public static void main (String[] args)
+    {
+        int studentAge = 17;
+        double studentGPA = 5.69;
+        char studentIntial = 'k';
+        String studentFinalName = "chandana";
+        char StudentFirstName = studentFinalName.charAt(0);
+        boolean hasPerfectAttandance = true;
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFinalName);
+        System.out.println(StudentFirstName);
+
+    }
+}
